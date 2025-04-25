3e8e1e4d4b738efab533b915d446e3b20da8878b
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-04-25 15:01:18 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "nareshawsdevvvv"
-    key    = "eks/terraform.tfstate"
+    bucket = "prakash-anand-bucket"
+    key    = "k8/terraform.tfstate"
     region = "us-east-1"
   }
 
